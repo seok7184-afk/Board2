0a6fd5d9460be7dba3b739060a42bd792a954620
seokkak <seok7184@gmail.com>
2026-04-23 12:55:19 +0900

    수정

---
+++ README.md
@@ -0,0 +1,7 @@
+### Board2 설정
+
+1. jdk 25
+2. oracle 21c express
+3. mybatis
+4. jsp
+# Board2
---
+++ src/test/java/com/green/board/BoardApplicationTests.java
@@ -0,0 +1,14 @@
+
+package com.green.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
